94765a013bfcf7eea2282848003156c477a55b7c
Joeri Bakker <bakkerjoeri@gmail.com>
2019-09-18 09:37:35 +0200

    Add empty state

--- src/components/BookmarkList.vue
+++ src/components/BookmarkList.vue
@@ -1,5 +1,5 @@
 <template>
-	<ul class="BookmarkList">
+	<ul class="BookmarkList" v-if="bookmarks.length">
 		<BookmarkItem
 			class="BookmarkList__item"
 			v-for="bookmark in bookmarks"
@@ -7,6 +7,9 @@
 			:bookmark="bookmark"
 		/>
 	</ul>
+	<div v-else>
+		<p class="BookmarkList__empty">No bookmarks here...</p>
+	</div>
 </template>
 
 <script>
@@ -23,5 +26,10 @@
 <style lang="scss">
 	.BookmarkList {
 		list-style: none;
+	}
+
+	.BookmarkList__empty {
+		font-style: italic;
+		color: gray;
 	}
 </style>
