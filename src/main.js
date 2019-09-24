eed8d87e61099933a3ebcc86a3739cf7e40444ce
Joeri Bakker <bakkerjoeri@gmail.com>
2019-09-24 09:26:43 +0200

    Add click outside and media queries

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue';
+import clickOutside from 'v-click-outside'
+import VueMq from 'vue-mq';
 import App from './App.vue';
 import store from './store';
 import router from './router';
+import breakpoints from './config/breakpoints.js';
 
 Vue.config.productionTip = false;
+Vue.use(clickOutside)
+Vue.use(VueMq, {
+	breakpoints,
+});
 
 new Vue({
 	store,
