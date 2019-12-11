c122233230e5e91d9dc8d921b33f2bdaa7f7a846
Joeri Bakker <bakkerjoeri@gmail.com>
2019-12-11 12:59:29 +0100

    Install firebase

--- src/main.js
+++ src/main.js
@@ -5,6 +5,19 @@ import App from './App.vue';
 import store from './store';
 import router from './router';
 import breakpoints from './config/breakpoints.js';
+import { initializeApp } from 'firebase/app';
+
+const CONFIG = {
+	apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+	authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+	projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGE_SENDER_ID,
+	appId: process.env.VUE_APP_FIREBASE_APP_ID,
+}
+
+initializeApp(CONFIG);
 
 Vue.config.productionTip = false;
 Vue.use(clickOutside)
