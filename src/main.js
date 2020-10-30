f336c0287204698320f03dbb4f0ea20c906e395b
Joeri Bakker <bakkerjoeri@gmail.com>
2020-10-31 00:47:27 +0100

    Comment out everything

--- src/main.js
+++ src/main.js
@@ -1,36 +1,36 @@
-import { createApp } from 'vue';
-import VueMq from 'vue3-mq';
-import App from './App.vue';
-import { store } from './store';
-import router from './router';
-import breakpoints from './config/breakpoints.js';
-import { auth } from './utility/firebase.js';
+// import { createApp } from 'vue';
+// import VueMq from 'vue3-mq';
+// import App from './App.vue';
+// import { store } from './store';
+// import router from './router';
+// import breakpoints from './config/breakpoints.js';
+// import { auth } from './utility/firebase.js';
 
-let hasAppMounted = false;
+// let hasAppMounted = false;
 
-function mountApp() {
-	const app = createApp(App);
+// function mountApp() {
+// 	const app = createApp(App);
 
-	app.use(store);
-	app.use(router);
-	app.use(VueMq, {
-		breakpoints,
-	});
+// 	app.use(store);
+// 	app.use(router);
+// 	app.use(VueMq, {
+// 		breakpoints,
+// 	});
 
-	app.mount('body');
-}
+// 	app.mount('body');
+// }
 
-auth.onAuthStateChanged(user => {
-	if (user) {
-		store.dispatch('login', user.uid);
-	} else if (store.getters.isLoggedIn) {
-		store.dispatch('logout');
-	} else {
-		store.dispatch('fetchLocalState');
-	}
+// auth.onAuthStateChanged(user => {
+// 	if (user) {
+// 		store.dispatch('login', user.uid);
+// 	} else if (store.getters.isLoggedIn) {
+// 		store.dispatch('logout');
+// 	} else {
+// 		store.dispatch('fetchLocalState');
+// 	}
 
-	if (!hasAppMounted) {
-		mountApp();
-		hasAppMounted = true;
-	}
-});
+// 	if (!hasAppMounted) {
+// 		mountApp();
+// 		hasAppMounted = true;
+// 	}
+// });
