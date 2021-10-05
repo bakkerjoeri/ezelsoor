507dd6fbfff10748303f6f14a4ab992d32b29820
Joeri Bakker <bakkerjoeri@gmail.com>
2021-10-05 20:48:58 +0200

    Fix dev experience for routes

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,12 +1,19 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  mount: {
-    public: '/',
-    src: '/dist',
-  },
-  plugins: [
-    '@snowpack/plugin-dotenv',
-    '@snowpack/plugin-svelte',
-    '@snowpack/plugin-typescript',
-  ],
+	mount: {
+		public: "/",
+		src: "/dist",
+	},
+	routes: [
+		{
+			match: "routes",
+			src: ".*",
+			dest: "/index.html",
+		},
+	],
+	plugins: [
+		"@snowpack/plugin-dotenv",
+		"@snowpack/plugin-svelte",
+		"@snowpack/plugin-typescript",
+	],
 };
