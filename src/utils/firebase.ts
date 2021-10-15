55e9c75489f1e40bb9b65dd2d4fda587a6efb573
Joeri Bakker <bakkerjoeri@gmail.com>
2021-10-15 18:14:43 +0200

    Remove console log

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,8 +3,6 @@ import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import getFirebaseConfig from "./getFirebaseConfig";
 
-console.log(getFirebaseConfig());
-
 const firebaseApp = initializeApp(getFirebaseConfig());
 const auth = getAuth(firebaseApp);
 const database = getFirestore(firebaseApp);
